145033d8e3b937b6e632cfce34cfdd7a10f08f99
Deepanshu <luckydeepanshu5@gmail.com>
2025-06-26 00:19:22 +0530

    binary search card problem

---
+++ Binary Search/CardProblem.py
@@ -0,0 +1,27 @@
+
+def located_cards(cards, query):
+    lo, hi = 0, len(cards)-1
+    
+    while lo <= hi:
+        mid = (lo + hi ) // 2
+        mid_val = cards[mid]
+        
+        if mid_val == query:
+            return mid
+        
+        if query < mid_val:
+            lo = mid + 1
+        else:
+            hi = mid -1
+            
+    return -1
+            
+    
+
+
+if __name__ == "__main__":
+    cards = [9,7,5,4,3,2,1]
+    query = 5
+    output = 1
+    result = located_cards(cards, query) == output
+    print(result)
